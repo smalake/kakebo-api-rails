7a24087ca9a27cc5f5828c664dab816db8d6c90c
smalake <41889150+smalake@users.noreply.github.com>
2023-08-26 16:31:12 +0900

    CORS対策

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000"
+    origins 'http://localhost:3000'
 
-    resource "*",
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head], :expose => ['access-token']
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      expose: ['access-token']
   end
 end
