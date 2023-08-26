028d98576d350199cd1b0194ab063be21a3ce1aa
smalake <41889150+smalake@users.noreply.github.com>
2023-08-26 23:23:13 +0900

    表示名を変更する機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/public', to: 'public#public'
-  get '/private', to: 'private#private'
   post '/login', to: 'login#login'
+
   post '/event', to: 'event#create'
   put '/event/:id', to: 'event#update'
   get '/event', to: 'event#get_all'
   get '/event/:id', to: 'event#get_one'
   delete '/event/:id', to: 'event#delete'
+
+  get '/display-name', to: 'setting#get'
+  put '/display-name', to: 'setting#update'
 end
