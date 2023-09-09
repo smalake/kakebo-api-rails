ea88694db078b6b466465ae6ef81a5300970c99f
smalake <41889150+smalake@users.noreply.github.com>
2023-09-09 23:33:57 +0900

    Userモデルの修正

--- db/migrate/20230825131850_create_users.rb
+++ db/migrate/20230825131850_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :uid
-      t.integer :group_id
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.integer :group_id, null: false
       t.string :name
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
