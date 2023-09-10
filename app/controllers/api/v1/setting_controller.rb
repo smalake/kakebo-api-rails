872815c7f3b579d232146184a0747319ca524629
smalake <41889150+smalake@users.noreply.github.com>
2023-09-10 14:52:48 +0900

    UIDではなくユーザのIDを使用するよう修正、APIモジュールの書き方を修正

--- app/controllers/api/v1/setting_controller.rb
+++ app/controllers/api/v1/setting_controller.rb
@@ -1,34 +1,30 @@
-module Api
-  module V1
-    class SettingController < ApplicationController
-      include Secured
+class Api::V1::SettingController < ApplicationController
+  include Secured
 
-      def get
-        begin
-          user = User.find_by(uid: @auth_user_id)
-          render json: { "name" => user.name }, status: :ok
-        rescue => e
-          render json: {
-                   message: "Failed to get Name",
-                   errors: e
-                 },
-                 status: :internal_server_error
-        end
-      end
+  def get
+    begin
+      user = User.find_by(id: @auth_user_id)
+      render json: { "name" => user.name }, status: :ok
+    rescue => e
+      render json: {
+               message: "Failed to get Name",
+               errors: e,
+             },
+             status: :internal_server_error
+    end
+  end
 
-      def update
-        begin
-          user = User.find_by(uid: @auth_user_id)
-          user.update(name: params[:name])
-          render json: { message: "Name update successfully" }, status: :ok
-        rescue => e
-          render json: {
-                   message: "Failed to update Name",
-                   errors: e
-                 },
-                 status: :unprocessable_entity
-        end
-      end
+  def update
+    begin
+      user = User.find_by(id: @auth_user_id)
+      user.update(name: params[:name])
+      render json: { message: "Name update successfully" }, status: :ok
+    rescue => e
+      render json: {
+               message: "Failed to update Name",
+               errors: e,
+             },
+             status: :unprocessable_entity
     end
   end
 end
