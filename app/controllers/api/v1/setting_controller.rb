2b40cbae62d2e6b44742e1672cedc75ee8200dc4
smalake <41889150+smalake@users.noreply.github.com>
2023-09-12 12:16:39 +0900

    招待リンク生成機能と、子としてユーザ登録機能を実装 #5

--- app/controllers/api/v1/setting_controller.rb
+++ app/controllers/api/v1/setting_controller.rb
@@ -27,4 +27,27 @@ class Api::V1::SettingController < ApplicationController
              status: :unprocessable_entity
     end
   end
+
+  # ログアウト
+  def logout
+    begin
+      cookies.delete :jwt
+      render status: :ok
+    rescue => e
+      render json: { message: e }, status: :internal_server_error
+    end
+  end
+
+  # グループへの招待リンクを発行
+  def invite
+    begin
+      user = User.find_by(id: @auth_user_id)
+      data = { group_id: user.group_id, parent_name: user.name }
+      token = JWT.encode({ data: data, exp: Time.current.since(10.minute).to_i }, ENV["TOKEN_SECRET"])
+
+      render json: { url: "#{ENV["FRONT_URL"]}/join/#{token}" }, status: :ok
+    rescue => e
+      render json: { message: "faild to generate invite link", error: e }, status: :internal_server_error
+    end
+  end
 end
