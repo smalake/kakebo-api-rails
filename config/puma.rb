4acd82a218de0357a2c5d5f2a37e6afcf93106cb
smalake <41889150+smalake@users.noreply.github.com>
2023-09-18 16:49:25 +0900

    ポート番号の変更

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ threads min_threads_count, max_threads_count
 rails_env = ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 8080 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 # environment rails_env
