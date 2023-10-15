e3a917a1663583074da43a63fd74eea2412ebcab
smalake <41889150+smalake@users.noreply.github.com>
2023-10-15 11:22:31 +0900

    groupテーブルにrevisionのカラムを追加 #34

--- db/migrate/20230825132922_create_groups.rb
+++ db/migrate/20230825132922_create_groups.rb
@@ -2,6 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :manage_user
+      t.integer :revision, :default => 0
 
       t.timestamps
     end
