3a2f569aa275c2c2930218c203bb5943ea2830d8
smalake <41889150+smalake@users.noreply.github.com>
2023-10-17 21:27:29 +0900

    認証コード送信部分の修正と、送信された認証コードの認証機能を実装 #36

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ApplicationMailer
   def auth_mail
-    random_number = rand(100000..999999)
-    @auth_code = "%06d" % random_number
     email = params[:email]
+    @auth_code = params[:auth_code]
     mail(to: email, subject: "【使いやすい家計簿】認証コード")
   end
 end
